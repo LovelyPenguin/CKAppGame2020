4f4d4164252b99a4c76c8808ea11254594b850cf
lyp3238 <lyp3238@gmail.com>
2020-05-08 19:26:09 +0900

    앱 나가고 나서의 시간 체크를 완성하고 임시 폰트를 추가함

--- Assets/Scripts/GameManager/TimeMng.cs
+++ Assets/Scripts/GameManager/TimeMng.cs
@@ -1,21 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeMng : MonoBehaviour
 {
+    public Text debugText;
+
     private void Awake()
     {
-        Debug.Log("Application Start");
+        Debug.Log(PlayerPrefs.GetString("SaveLastTime"));
     }
 
     private void OnApplicationPause(bool pause)
     {
-        Debug.Log("Application Pause");
+        if (pause)
+        {
+            PlayerPrefs.SetString("SaveLastTime", System.DateTime.Now.ToString());
+        }
+        else
+        {
+            string lastTime = PlayerPrefs.GetString("SaveLastTime");
+            System.DateTime lastDataTime = System.DateTime.Parse(lastTime);
+            System.TimeSpan compareTime = System.DateTime.Now - lastDataTime;
+
+            float day = compareTime.Days * 86400;
+            float hour = compareTime.Hours * 3600;
+            float min = compareTime.Minutes * 60;
+            float sec = compareTime.Seconds;
+            Debug.Log(compareTime);
+            Debug.Log(hour + min + sec);
+
+            if (debugText != null)
+            {
+                float time = hour + min + sec;
+                debugText.text = "마지막 접속으로 부터 : " + time + " sec";
+            }
+        }
     }
 
     private void OnDestroy()
     {
-        Debug.Log("Application Close");
+        PlayerPrefs.SetString("SaveLastTime", System.DateTime.Now.ToString());
+        Debug.Log(PlayerPrefs.GetString("SaveLastTime"));
+    }
+
+    public void ResetAllData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+    
+    public void ResetData(string keyValue)
+    {
+        PlayerPrefs.DeleteKey(keyValue);
     }
 }
