a0535b30760ef0e2b89edc6791ecc95be62cabc4
lyp3238 <lyp3238@gmail.com>
2020-05-17 01:00:41 +0900

    임시용 모델과 Nav 메시 적용

---
+++ Assets/Scripts/CharacterControl/GameObjectContor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectContor : MonoBehaviour
+{
+    public float speed = 3;
+    private Vector3 playerPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerPosition.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerPosition.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerPosition.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            playerPosition.z -= speed * Time.deltaTime;
+        }
+
+        transform.position = playerPosition;
+    }
+}
---
+++ Assets/Scripts/CharacterControl/RandomMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RandomMove : MonoBehaviour
+{
+    public NavMeshAgent nav;
+    public GameObject target;
+    [SerializeField]
+    private Vector3 targetPostion;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private bool isArrive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPostion = target.transform.position;
+
+        targetPostion.x = Random.Range(-15, 15);
+        targetPostion.z = Random.Range(-15, 15);
+        nav.SetDestination(targetPostion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            targetPostion = target.transform.position;
+        }
+        
+        if (Vector3.Distance(transform.position, nav.destination) <= 0.3f)
+        {
+            isArrive = true;
+            targetPostion.x = Random.Range(-15, 15);
+            targetPostion.z = Random.Range(-15, 15);
+            nav.SetDestination(targetPostion);
+        }
+        else
+        {
+            isArrive = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(targetPostion, 0.1f);
+    }
+}
