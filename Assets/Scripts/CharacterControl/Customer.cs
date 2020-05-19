fed4f21b00233c443715913071342324569ed929
lyp3238 <lyp3238@gmail.com>
2020-05-19 15:28:05 +0900

    손님 스크립트 생성

---
+++ Assets/Scripts/CharacterControl/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    public bool isOpen;
+    public float currentCafeTime;
+    public int duration;
+
+    private float durationSecond;
+    // Start is called before the first frame update
+    void Start()
+    {
+        durationSecond = duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OpenCheck();
+        DurationCheck();
+
+    }
+
+    void OpenCheck()
+    {
+        isOpen = GameMng.Instance.getOpenData;
+        currentCafeTime = GameMng.Instance.openTime;
+    }
+
+    void DurationCheck()
+    {
+        if (isOpen)
+        {
+            durationSecond -= Time.deltaTime;
+        }
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
+            
+        }
+        else
+        {
+            durationSecond -= GameMng.Instance.GetComponent<TimeMng>().flowTime;
+            Debug.Log(GameMng.Instance.GetComponent<TimeMng>().flowTime);
+        }
+    }
+}
