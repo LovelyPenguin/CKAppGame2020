4412438c0dff9f6fcc7986eac13796d085d0d35e
lyp3238 <lyp3238@gmail.com>
2020-05-19 15:27:14 +0900

    움직이는 방향에 따라 이미지 변형 기능 추가

---
+++ Assets/Scripts/CharacterControl/AutoSpriteFlip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSpriteFlip : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    public bool isLeftInitialize;
+    public string direction;
+
+    private float previousXpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousXpos = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (previousXpos <= transform.position.x)
+        {
+            direction = "Right";
+            sprite.flipX = isLeftInitialize;
+        }
+        else
+        {
+            direction = "Left";
+            sprite.flipX = !isLeftInitialize;
+        }
+
+        previousXpos = transform.position.x;
+    }
+}
