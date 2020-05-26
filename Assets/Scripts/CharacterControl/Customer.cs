34d1a2677b5b9365d242b185efab8bcb9bde8b6c
lyp3238 <lyp3238@gmail.com>
2020-05-26 21:42:42 +0900

    카페 오픈 여부에 따라 손님 AI가 행동하도록 수정함

--- Assets/Scripts/CharacterControl/Customer.cs
+++ Assets/Scripts/CharacterControl/Customer.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental.FileFormat;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Customer : MonoBehaviour
 {
     public bool isOpen;
+    public bool isActive = false;
     public float currentCafeTime;
     public int duration;
+    public float activeTime;
+    public Vector3 poolingPos;
+    public Vector3 entrancePos;
 
     private float durationSecond;
     // Start is called before the first frame update
     void Awake()
     {
         Debug.Log("START");
-        durationSecond = duration * 60;
+    }
+
+    private void Start()
+    {
+        //durationSecond = duration * 60;
+        durationSecond = duration;
+        GameMng.Instance.openEvent.AddListener(InitializeSpawnData);
+        gameObject.transform.position = poolingPos;
+        gameObject.GetComponent<NavMeshAgent>().enabled = false;
     }
 
     // Update is called once per frame
@@ -21,6 +35,10 @@ public class Customer : MonoBehaviour
     {
         OpenCheck();
         DurationCheck();
+
+        if (isActive == true)
+        {
+        }
     }
 
     void OpenCheck()
@@ -30,20 +48,59 @@ public class Customer : MonoBehaviour
 
         if (isOpen == false)
         {
-
+            isActive = false;
         }
         else
         {
-
+            if (isActive == false)
+            {
+                activeTime -= Time.deltaTime;
+            }
+            if (activeTime <= 0)
+            {
+                //Debug.Log("Active");
+                isActive = true;
+                InitializeSpawnData();
+                gameObject.transform.position = entrancePos;
+                gameObject.GetComponent<NavMeshAgent>().enabled = true;
+            }
         }
     }
 
     void DurationCheck()
     {
-        if (isOpen)
+        if (isOpen && isActive)
         {
             durationSecond -= Time.deltaTime;
+
+            if (durationSecond <= 0)
+            {
+
+                gameObject.GetComponent<NavMeshAgent>().SetDestination(entrancePos);
+
+                if (Vector3.Distance(gameObject.transform.position, gameObject.GetComponent<NavMeshAgent>().destination) <= 1f)
+                {
+                    gameObject.transform.position = poolingPos;
+                    gameObject.GetComponent<NavMeshAgent>().enabled = false;
+                    durationSecond = duration;
+                    isActive = false;
+                }
+            }
         }
+        else
+        {
+            isActive = false;
+        }
+    }
+
+    void Spawn()
+    {
+        isActive = true;
+    }
+
+    void InitializeSpawnData()
+    {
+        activeTime = Random.Range(5, 25);
     }
 
     private void OnApplicationPause(bool pause)
@@ -58,5 +115,14 @@ public class Customer : MonoBehaviour
             durationSecond -= GameMng.Instance.GetComponent<TimeMng>().flowTime;
             Debug.Log(GameMng.Instance.GetComponent<TimeMng>().flowTime);
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(poolingPos, 0.5f);
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawSphere(entrancePos, 0.5f);
+        Gizmos.color = Color.yellow;
     }
 }
