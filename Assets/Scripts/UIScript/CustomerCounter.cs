7a1edbe1ff1c1b821f178049f101b230557aba86
lyp3238 <lyp3238@gmail.com>
2020-06-07 22:14:03 +0900

    씬 합침

---
+++ Assets/Scripts/UIScript/CustomerCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerCounter : MonoBehaviour
+{
+    private Text counterText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counterText = gameObject.GetComponent<Text>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counterText.text = "들린 손님 : " + GameMng.Instance.customerCount.ToString();
+    }
+}
