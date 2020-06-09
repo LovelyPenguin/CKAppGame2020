6ce1b8ba3e1e0bb826fde900eaa347553acc7b33
lyp3238 <lyp3238@gmail.com>
2020-06-09 10:11:57 +0900

    신규 리소스 추가

--- Assets/Scripts/ButtonTest.cs
+++ Assets/Scripts/ButtonTest.cs
@@ -1,23 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonTest : MonoBehaviour
 {
+    [Range(0, 15), SerializeField]
+    private float timer;
+    private Image img;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        img = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void Click()
-    {
-        Debug.Log("CLICK!");
+        img.fillAmount = 1 / (GameMng.Instance.setOpenTime / GameMng.Instance.openTime);
     }
 }
