056b93e62463cde0ee1eae2fc5baa94e97c90787
lyp3238 <lyp3238@gmail.com>
2020-06-16 11:09:30 +0900

    휴대폰 물리키를 감지하는 스크립트 원형 추가

---
+++ Assets/Scripts/GameManager/MobileInputMng.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInputMng : MonoBehaviour
+{
+    public float timer = 0.1f;
+    private bool key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        key = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            key = true;
+        }
+        if (key)
+        {
+            timer -= Time.deltaTime;
+            if (timer >= 0)
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    Debug.Log("Click");
+                }
+            }
+            else
+            {
+                key = false;
+                timer = 0.1f;
+            }
+        }
+    }
+}
