435bede4cdb470cdbec9aa93eebef3717e717867
lyp3238 <lyp3238@gmail.com>
2020-06-17 19:36:52 +0900

    일시정지 기능 제작

---
+++ Assets/Scripts/UIScript/PauseMenu/PauseButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pausePopup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pausePopup == null)
+        {
+            pausePopup = GameObject.Find("PauseMenu");
+        }
+        else
+        {
+            Debug.LogError("Pause Menu Not found!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenPauseMenu()
+    {
+        pausePopup.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UIScript/PauseMenu/ResumeButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResumeButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pausePopup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pausePopup = GetComponentInParent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResumeGame()
+    {
+        pausePopup.SetActive(false);
+    }
+}
