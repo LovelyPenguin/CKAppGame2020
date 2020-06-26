ecca6e68d36d810cf941ee590a2cc0acc7c6c6fa
lyp3238 <lyp3238@gmail.com>
2020-06-26 23:16:24 +0900

    아이템 기능 제작

---
+++ Assets/Scripts/CharacterControl/CustomerItemInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerItemInfo : MonoBehaviour
+{
+    public GameObject host;
+    public bool isActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
