adf2bb0a166552d270f6268c22ed68a012922ac1
lyp3238 <lyp3238@gmail.com>
2020-07-05 22:28:50 +0900

    언락기능 완성

--- Assets/Scripts/CharacterControl/CheckCustomerAppear.cs
+++ Assets/Scripts/CharacterControl/CheckCustomerAppear.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class CheckCustomerAppear : MonoBehaviour
 {
     public int condition;
+    public RaccoonController[] rac;
+
+    private bool racOption;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        if (rac.Length == 0)
+        {
+            racOption = true;
+        }
+        else
+        {
+            racOption = false;
+        }
     }
 
     // Update is called once per frame
@@ -20,6 +30,18 @@ public class CheckCustomerAppear : MonoBehaviour
 
     public bool CheckAppear()
     {
+        if (!racOption)
+        {
+            return NoRac();
+        }
+        else
+        {
+            return Rac();
+        }
+    }
+
+    private bool NoRac()
+    {
         if (condition <= PlayerPrefs.GetInt("CUSTOMERCOUNT"))
         {
             return true;
@@ -28,5 +50,22 @@ public class CheckCustomerAppear : MonoBehaviour
         {
             return false;
         }
+    }
+
+    private bool Rac()
+    {
+        bool returnValue = false;
+        for (int i = 0; i < rac.Length; i++)
+        {
+            if (condition <= PlayerPrefs.GetInt("CUSTOMERCOUNT") && (rac[i].GetRCState == RaccoonController.State.inMap1 || rac[i].GetRCState == RaccoonController.State.inMap2))
+            {
+                returnValue = true;
+            }
+            else if (condition > PlayerPrefs.GetInt("CUSTOMERCOUNT") && (rac[i].GetRCState == RaccoonController.State.inMap1 || rac[i].GetRCState == RaccoonController.State.inMap2))
+            {
+                returnValue = false;
+            }
+        }
+        return returnValue;
     }
 }
---
+++ Assets/Scripts/CharacterControl/CustomerUnlock.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerUnlock : MonoBehaviour
+{
+    public JuiceData[] jui;
+    public RaccoonController[] rac;
+
+    private bool juiUnlock = false;
+    private bool racUnlock = false;
+
+    private bool debugJui;
+    private bool debugRac;
+
+    private Customer cus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (jui.Length == 0)
+        {
+            juiUnlock = true;
+        }
+        else
+        {
+            juiUnlock = false;
+        }
+
+        if (rac.Length == 0)
+        {
+            racUnlock = true;
+        }
+        else
+        {
+            racUnlock = false;
+        }
+
+        cus = GetComponent<Customer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cus.unlock = TotalUnlockCheck();
+    }
+
+    public bool TotalUnlockCheck()
+    {
+        if (RacUnlockCheck() && JuiUnlockCheck())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool RacUnlockCheck()
+    {
+        bool bol = false;
+
+        if (racUnlock)
+        {
+            return true;
+            debugRac = true;
+        }
+        else
+        {
+            for (int i = 0; i < rac.Length; i++)
+            {
+                if (rac[i].GetRCState != RaccoonController.State.unActive)
+                {
+                    bol = true;
+                    debugRac = true;
+                }
+                else
+                {
+                    bol = false;
+                    debugRac = false;
+                }
+            }
+        }
+        return bol;
+    }
+
+    private bool JuiUnlockCheck()
+    {
+        bool bol = false;
+
+        if (juiUnlock)
+        {
+            return true;
+            debugJui = true;
+        }
+        else
+        {
+            for (int i = 0; i < rac.Length; i++)
+            {
+                if (jui[i].isUnlock)
+                {
+                    bol = true;
+                    debugJui = true;
+                }
+                else
+                {
+                    bol = false;
+                    debugJui = false;
+                }
+            }
+        }
+        return bol;
+    }
+}
