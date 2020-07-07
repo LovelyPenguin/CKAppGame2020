7ff6b3043bc5a09d0c2a23b8ba6ec2cb0dd75d66
lyp3238 <lyp3238@gmail.com>
2020-07-07 14:08:34 +0900

    게임 꺼졌을때 대응하는 스크립트 추가

---
+++ Assets/Scripts/GameManager/GameQuitAndPauseEvent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameQuitAndPauseEvent : MonoBehaviour
+{
+    public UnityEvent gameExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("Application Quit");
+        gameExit.Invoke();
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
+            Debug.Log("Application Pause");
+        }
+        else
+        {
+            Debug.Log("Application Rasume");
+        }
+    }
+}
