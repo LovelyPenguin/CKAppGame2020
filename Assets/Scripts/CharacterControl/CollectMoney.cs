6ff5baf6f53c3efa555f45a86e2863713bfe23f8
lyp3238 <lyp3238@gmail.com>
2020-07-07 09:55:58 +0900

    - 손님을 통해 재화 획득 시 사운드 출력 - 꼬마 리소스 변경

--- Assets/Scripts/CharacterControl/CollectMoney.cs
+++ Assets/Scripts/CharacterControl/CollectMoney.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CollectMoney : MonoBehaviour
 {
+    public AudioClip clip;
+    private AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = transform.GetComponentInParent<AudioSource>();
+        source.clip = clip;
     }
 
     // Update is called once per frame
