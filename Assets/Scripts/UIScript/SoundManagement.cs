f64f5bb19f1d353801f838763b50ff86b24fa0f1
lyp3238 <lyp3238@gmail.com>
2020-07-07 11:54:47 +0900

    음료 제작 사운드 적용

---
+++ Assets/Scripts/UIScript/SoundManagement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagement : MonoBehaviour
+{
+    public AudioClip[] clips;
+
+    private AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        int random = Random.Range(0, clips.Length - 1);
+        audio.clip = clips[random];
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+        }
+    }
+}
