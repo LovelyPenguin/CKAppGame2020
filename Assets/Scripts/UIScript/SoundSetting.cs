b0ffa1e6a2fffcf4af365a351c26425e01496f8b
lyp3238 <lyp3238@gmail.com>
2020-07-11 15:31:21 +0900

    옵션창 제작

---
+++ Assets/Scripts/UIScript/SoundSetting.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSetting : MonoBehaviour
+{
+    private bool sound = true;
+    private AudioListener al;
+    private Text childText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        al = Camera.main.GetComponent<AudioListener>();
+        childText = transform.GetChild(0).GetComponent<Text>();
+
+        if (sound)
+        {
+            childText.text = "사운드 : 끄기";
+        }
+        else
+        {
+            childText.text = "사운드 : 켜기";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        sound = !sound;
+        al.enabled = sound;
+
+        if (sound)
+        {
+            childText.text = "사운드 : 끄기";
+        }
+        else
+        {
+            childText.text = "사운드 : 켜기";
+        }
+    }
+}
