ac97995f531435c3ba9f5d3d6caf8b01a175845e
lyp3238 <lyp3238@gmail.com>
2020-07-12 21:52:20 +0900

    수정사항 적용 및 치트 시스템 적용

---
+++ Assets/Scripts/UIScript/CheatSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatSystem : MonoBehaviour
+{
+    private int count1;
+    private int count2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count1 = 0;
+        count2 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Count1Add()
+    {
+        count1++;
+        if (count1 >= 4)
+        {
+            count1 = 0;
+        }
+    }
+    public void Count2Add()
+    {
+        if (count1 == 3)
+        {
+            count2++;
+            if (count2 == 3)
+            {
+                Debug.Log("Cheat Start");
+                count1 = 0;
+                count2 = 0;
+                GameMng.Instance.money = int.MaxValue;
+            }
+        }
+        else
+        {
+            count1 = 0;
+            count2 = 0;
+        }
+    }
+}
